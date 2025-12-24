4403a06c170af4acaa95132d1b4258fce72b8885
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-12-24 10:25:33 +0800

    feat: add keyboard accessibility

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,18 @@ import '../styles/card.css';
 
 export default function Card({ img, name, onClick }) {
   return (
-    <div className="card" onClick={onClick}>
-      <img src={img} alt="" />
+    <div
+      className="card"
+      onClick={onClick}
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault(); // prevent scrolling for space
+          onClick();
+        }
+      }}
+    >
+      <img src={img} alt={name} />
       <h2>{name}</h2>
     </div>
   );
